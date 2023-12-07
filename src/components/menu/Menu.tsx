1b6bdd9bfa4683942bbf35664559dc4b45d9d846
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-12-07 12:00:35 +0300

    Добавляет header

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Logo } from '../../components/logo/Logo';
+
+export const Menu = () => {
+  return (
+    <StyledMenu>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About me</a>
+        </li>
+        <li>
+          <a href="">Skills</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Contacts</a>
+        </li>
+      </ul>
+    </StyledMenu>
+  );
+};
+
+const StyledMenu = styled.nav`
+  ul {
+    display: flex;
+    gap: 30px;
+  }
+`
