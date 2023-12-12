d2a6da6f3f55039e527d2b5c3034e837e422e07e
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-12-12 14:08:03 +0300

    удаляет wrapper

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,48 +1,48 @@
 import React from 'react';
-import { FlexWrapper } from '../../components/FlexWrapper';
 import styled from 'styled-components';
 import { Icon } from '../../components/icon/Icon';
 
 export const Footer = () => {
   return (
     <StyledFooter>
-      <FlexWrapper justify={"space-between"}>
-        <AddressList>
-          <AddressLink href={"tel:+375298066793"}>+375298066793</AddressLink>
-          <AddressLink href={"mailto:nadya-manko@mail.ru"}>nadya-manko@mail.ru</AddressLink>
-        </AddressList>
-        <SocialList>
-          <SocialItem>
-            <SocialLink href={"https://t.me/@nadya31121990"}>
-              <Icon iconId={"telegram"} width={"40px"} height={"34px"} viewBox={"0 0 40 34"}/>
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink href={"https://github.com/Nadin3112"}>
-              <Icon iconId={"gith"} width={"40px"} height={"34px"} viewBox={"0 0 40 34"}/>
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink href={"https://www.instagram.com/Nadin3112/"}>
-              <Icon iconId={"instagram"} width={"40px"} height={"34px"} viewBox={"0 0 40 34"}/>
-            </SocialLink>
-          </SocialItem>
-        </SocialList>
-      </FlexWrapper>
+      <AddressList>
+        <AddressLink href={"tel:+375298066793"}>+375298066793</AddressLink>
+        <AddressLink href={"mailto:nadya-manko@mail.ru"}>nadya-manko@mail.ru</AddressLink>
+      </AddressList>
+      <SocialList>
+        <SocialItem>
+          <SocialLink href={"https://t.me/@nadya31121990"}>
+            <Icon iconId={"telegram"} width={"40px"} height={"34px"} viewBox={"0 0 40 34"} />
+          </SocialLink>
+        </SocialItem>
+        <SocialItem>
+          <SocialLink href={"https://github.com/Nadin3112"}>
+            <Icon iconId={"gith"} width={"40px"} height={"34px"} viewBox={"0 0 40 34"} />
+          </SocialLink>
+        </SocialItem>
+        <SocialItem>
+          <SocialLink href={"https://www.instagram.com/Nadin3112/"}>
+            <Icon iconId={"instagram"} width={"40px"} height={"34px"} viewBox={"0 0 40 34"} />
+          </SocialLink>
+        </SocialItem>
+      </SocialList>
       <Copyright>© 2023 Nadia Padalitsa, All Rights Reserved.</Copyright>
     </StyledFooter>
   );
 };
 
 const StyledFooter = styled.footer`
+  display:  flex;
+  justify-content: space-between;
   padding: 10px;
   background-color: #080357;
+  color: #ffffff;
 `
 const AddressList = styled.address`
 
 `
 const AddressLink = styled.a`
-    color: white;
+  color: #ffffff;
   &:not(:last-child) {
     margin-right: 20px;
   }
@@ -58,6 +58,4 @@ const SocialLink = styled.a`
 
 `
 const Copyright = styled.p`
-  text-align: center;
-  color: #ffffff;
 `
