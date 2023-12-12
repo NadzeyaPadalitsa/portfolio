f52eadf8a8722d77a923300d60b68dbd8b3f974c
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-12-12 15:10:33 +0300

    Добавляет ссылку для лого

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,7 @@ import { Icon } from '../icon/Icon';
 
 export const Logo = () => {
   return (
-    <a href={"#"}>
+    <a href={"index.html"}>
       <Icon iconId = {"logo"} width={"185"} height={"48"} viewBox={"0 0 185 48"} />
     </a>
   );
