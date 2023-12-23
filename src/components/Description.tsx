c24aeabc234231ee0b4b261b2e12102a96126ec6
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-12-23 16:18:34 +0300

    Изменяет стили

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,15 +1,7 @@
 import styled from "styled-components";
-import { theme } from "../styles/Theme";
 
 export const Description = styled.p`
-  margin-bottom: 20px;
   padding-left: 30px;
   letter-spacing: 0.72px;
   line-height: 1.4;
-
-  @media ${theme.media.mobile} {
-    max-width: 340px;
-    padding-left: 0;
-    width: 100%;
-  }
 `
