2f2dc3ae38870d713d824a81082c124db3ed3077
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2023-12-24 18:44:27 +0300

    Добавляет открытие и закрытие мобильного меню

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from '../menu/Menu';
 import { S } from '../HeaderMenu_Styles';
 
 export const MobileMenu: React.FC = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const onBurgerBtnClick = () => {setMenuIsOpen(!menuIsOpen)}
   return (
     <S.MobileMenu>
-      <S.BurgerButton aria-label="open menu" isOpen={false}>
+      <S.BurgerButton aria-label="open menu" isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
       </S.BurgerButton>
-      <S.MobileMenuPopup isOpen={false}>
+      <S.MobileMenuPopup isOpen={menuIsOpen} onClick = {() => {setMenuIsOpen(false)}}>
       <Menu/>
       </S.MobileMenuPopup>
     </S.MobileMenu>
